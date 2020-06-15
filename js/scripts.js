5e012e5db58e16840777a40f95487037bc9286c9
Megan Hepner <megan.hepner@gmail.com>
2020-06-15 09:43:04 -0700

    add content to Places objects
    Co-Authored-By: Ben Russell <benjaminrussell36@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,14 @@ $(document).ready(function() {
 // Business Logic
 function Place(location, landmark, timeOfYear) {
   this.location = location;
-  this.landmark
+  this.landmark = landmark;
+  this.timeOfYear = timeOfYear;
 }
+
+let portland = new Place("Portland", "Forest Park", "Fall");
+let seattle = new Place("Seattle", "Pike Place Market", "Spring");
+let barcelona = new Place("Barcelona", "Park Guell", "Summer");
+let paris = new Place("Paris", "Notre Dame", "Winter");
+let sydney = new Place("Sydney", "Opera House", "Spring");
+let kangarooIsland = new Place("Kangaroo Island", "Eucalyptus Forest", "Summer");
+let auckland = new Place("Auckland", "Waiheke Island", "Spring");
