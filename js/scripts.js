3f3ea436a124ba64434211c0b9c4aae68ea39b68
Megan Hepner <megan.hepner@gmail.com>
2020-06-15 10:06:44 -0700

    create click function that returns place values for Portland
    Co-Authored-By: Ben Russell <benjaminrussell36@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // UI Logic
 $(document).ready(function() {
-  
+  $("ul").click(function () 
+  {$("#portland").append("<li>" + portland.location + ", " + portland.landmark + ", " + portland.timeOfYear + "</li>")});
 });
 
 // Business Logic
@@ -10,10 +11,11 @@ function Place(location, landmark, timeOfYear) {
   this.timeOfYear = timeOfYear;
 }
 
-let portland = new Place("Portland", "Forest Park", "Fall");
+let portland = new Place("Portland", "Forest Park", "Fall");z
 let seattle = new Place("Seattle", "Pike Place Market", "Spring");
 let barcelona = new Place("Barcelona", "Park Guell", "Summer");
 let paris = new Place("Paris", "Notre Dame", "Winter");
 let sydney = new Place("Sydney", "Opera House", "Spring");
 let kangarooIsland = new Place("Kangaroo Island", "Eucalyptus Forest", "Summer");
 let auckland = new Place("Auckland", "Waiheke Island", "Spring");
+
