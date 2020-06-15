a49ddd3145c2e076e09034c16b3292f336056851
Megan Hepner <megan.hepner@gmail.com>
2020-06-15 11:09:31 -0700

    push all new Place information into TravelLog array
     Co-Authored-By: Ben Russell <benjaminrussell36@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,30 @@
 // UI Logic
 $(document).ready(function() {
-  $("ul").click(function () 
-  {$("#portland").append("<li>" + portland.location + ", " + portland.landmark + ", " + portland.timeOfYear + "</li>")});
+  $("ul").click(function () {
+  });
 });
 
 // Business Logic
+function TravelLog() {
+  this.places = [];
+}
+
+TravelLog.prototype.addPlace = function(place) {
+  this.places.push(place);
+}
+
 function Place(location, landmark, timeOfYear) {
   this.location = location;
   this.landmark = landmark;
   this.timeOfYear = timeOfYear;
 }
 
-let portland = new Place("Portland", "Forest Park", "Fall");z
+Place.prototype.fullInfo = function() {
+  return this.location + ", " + this.landmark + ", " + this.timeOfYear;
+}
+
+let travelLog = new TravelLog();
+let portland = new Place("Portland", "Forest Park", "Fall");
 let seattle = new Place("Seattle", "Pike Place Market", "Spring");
 let barcelona = new Place("Barcelona", "Park Guell", "Summer");
 let paris = new Place("Paris", "Notre Dame", "Winter");
@@ -19,3 +32,12 @@ let sydney = new Place("Sydney", "Opera House", "Spring");
 let kangarooIsland = new Place("Kangaroo Island", "Eucalyptus Forest", "Summer");
 let auckland = new Place("Auckland", "Waiheke Island", "Spring");
 
+
+travelLog.addPlace(portland);
+travelLog.addPlace(seattle);
+travelLog.addPlace(barcelona);
+travelLog.addPlace(paris);
+travelLog.addPlace(sydney);
+travelLog.addPlace(kangarooIsland);
+travelLog.addPlace(auckland);
+console.log(travelLog.places);
